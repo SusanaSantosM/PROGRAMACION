6fc4766374a36db0d0b660485dbe1bc7d19ee8a8
Susana Santos <ssantosmallqui@gmail.com>
2023-10-08 02:37:34 +0200

    ejercicio 1-2-3-4-5

---
+++ Boletin02/Boletin02_1/src/Boletin02_1.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Boletin02_1 {
+    public static void main(String[] args) {
+    //Introducimos datos
+        float base=4, altura=3, area;
+
+        //Calculamos
+        area=base*altura/2;
+
+        //Mostramos
+        System.out.println("area="+area);
+    }
+}
