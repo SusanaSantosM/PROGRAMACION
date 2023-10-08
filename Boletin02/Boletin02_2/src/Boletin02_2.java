e17a6617808db03b426d15afb367699e8d35b997
Susana Santos <ssantosmallqui@gmail.com>
2023-10-08 02:55:07 +0200

    ejercicio area cuadrado

---
+++ Boletin02/Boletin02_2/src/Boletin02_2.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Boletin02_2 {
+    public static void main(String[] args) {
+    //Añadimos datos
+        float lado=3, area;
+
+    //Calculamos area
+        area=lado*lado;
+    //otra forma
+        //area=(float)Math.pow(lado,2);
+
+    //Mostramos
+        System.out.println("area="+area);
+
+    //Sin declarar area seria ("area="+lado*lado)
+    }
+}
