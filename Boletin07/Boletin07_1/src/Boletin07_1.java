91e0d663aafdf84b053cee0a10640bad5c44cbee
Susana Santos <ssantosmallqui@gmail.com>
2023-10-18 22:30:28 +0200

    numero positivo

---
+++ Boletin07/Boletin07_1/src/Boletin07_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Boletin07_1 {
+    public static void main(String[] args) {
+        Numero ob = new Numero();
+
+        Scanner sc = new Scanner(System.in);
+
+        ob.setNumero(sc.nextInt());
+
+    }
+}
