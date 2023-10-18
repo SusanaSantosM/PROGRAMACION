eb9f00a600f018d266a46715d1769304c49f10b6
Susana Santos <ssantosmallqui@gmail.com>
2023-10-18 22:57:24 +0200

    numero positivo

---
+++ Boletin07/Boletin07_1/Boletin07_1/src/Boletin07_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Boletin07_1 {
+
+    public static void main(String[] args) {
+
+        NumPositivo ob = new NumPositivo();
+
+        Scanner sc = new Scanner(System.in);
+
+        ob.setNumPositivo(sc.nextInt());
+
+    }
+}
