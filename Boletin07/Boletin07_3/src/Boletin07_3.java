8c97bdb27161dc84c9f46e2f1c2a83695b831c00
ssusibel <dam1@aulaAulainformatica1>
2023-10-20 13:25:48 +0200

    condiconales

---
+++ Boletin07/Boletin07_3/src/Boletin07_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Boletin07_3 {
+    public static void main(String[] args) {
+        Numero ob = new Numero();
+        System.out.println("ingresa valor de n");
+
+        Scanner sc = new Scanner(System.in);
+        ob.setNumero(sc.nextInt());
+
+    }
+}
---
+++ Boletin07/Boletin07_3/src/Numero.java
@@ -0,0 +1,11 @@
+public class Numero {
+    public void setNumero(int n){
+        if(n>0)
+            System.out.println("es un número + ");
+        else if(n<0)
+            System.out.println("es un número - ");
+        else
+            System.out.println("es de valor 0 ");
+        System.out.println("finito");
+    }
+}
