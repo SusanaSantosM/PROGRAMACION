ec6fbf689203e00c1285544338d2c1f22ee9e905
Susana Santos <ssantosmallqui@gmail.com>
2023-12-05 00:16:30 +0100

    clase main

---
+++ Boletin17/src/Boletin17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Boletin17 {
+    public static void main(String[] args) {
+        Persoa per =  new Persoa("Karla","Aguilar",45962);
+
+        Scanner sc = new Scanner(System.in);
+        //ponemos el sop antes para indicar lo que pedimos
+        System.out.println("Ingrese numero cuenta: ");
+        //ponemos sc.nextlong() para poder sobreescribir el dato.
+        Conta c = new Conta(sc.nextLong(),per);
+
+        Scanner monto = new Scanner(System.in);
+        System.out.println("Ingrese el monto del dinero: ");
+        c.ingresar(monto.nextDouble());
+        System.out.println("Ingrese el monto que quiere retirar: ");
+        c.retirar(monto.nextDouble());
+
+    }
+}
