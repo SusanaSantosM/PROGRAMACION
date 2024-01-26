c9280667efc2c4d287c45d6a93264388914e9f83
ssusibel <dam1@aulaAulainformatica1>
2024-01-26 11:37:17 +0100

    Dias de semana

---
+++ ejer_array_2/Ejer_Arrays2.java
@@ -0,0 +1,13 @@
+
+package ejer_arrays2;
+
+import javax.swing.JOptionPane;
+
+public class Ejer_Arrays2 {
+
+    public static void main(String[] args) {
+        DiasDeSemana n = new DiasDeSemana ();
+        n.mostrarDia(Integer.parseInt(JOptionPane.showInputDialog("Escribe un número del 1 al 7:")));
+    }
+    
+}
