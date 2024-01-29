6d14d56cfc919752acd1c121ec4b4723d19f77c5
ssusibel <dam1@aulaAulainformatica1>
2024-01-29 11:26:59 +0100

    mostramos los cantos

--- Boletin19/Boletin19.java
+++ Boletin19/Boletin19.java
@@ -4,7 +4,12 @@ package boletin19;
 public class Boletin19 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+                // Visualizamos
+        IPoderCantar Andre = new Persoa();
+         Andre.Cantar();
+
+        IPoderCantar ave = new Canario();
+         ave.Cantar();
     }
     
 }
