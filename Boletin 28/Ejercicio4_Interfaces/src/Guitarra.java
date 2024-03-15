3f652bca165a6d45f964c2658052be41652b69ce
SantosMSusana <145323629+SusanaSantosM@users.noreply.github.com>
2024-03-15 02:06:24 +0100

    Delete Boletin 28 directory

--- Boletin 28/Ejercicio4_Interfaces/src/Guitarra.java
+++
@@ -1,7 +0,0 @@
-public class Guitarra implements InstrumentoMusical{
-
-    @Override
-    public void tocar() {
-        System.out.println("Tocando la guitarra");
-    }
-}
--- Boletin 28/Ejercicio4_Interfaces/src/Piano.java
+++
@@ -1,7 +0,0 @@
-public class Piano implements InstrumentoMusical{
-
-    @Override
-    public void tocar() {
-        System.out.println("Tocando piano");
-    }
-}
