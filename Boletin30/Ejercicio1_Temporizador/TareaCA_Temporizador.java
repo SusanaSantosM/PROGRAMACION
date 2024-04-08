3059f75c8ce5edef74243acb3a332a169adfb560
SantosMSusana <145323629+SusanaSantosM@users.noreply.github.com>
2024-04-08 11:41:35 +0200

    USAMOS CLASES ANONIMAS

---
+++ Boletin30/Ejercicio1_Temporizador/TareaCA_Temporizador.java
@@ -0,0 +1,12 @@
+
+package tareaca_temporizador;
+
+public class TareaCA_Temporizador {
+
+    public static void main(String[] args) {
+        // Visualizamos
+        Temporizador tem = new Temporizador();
+        tem.setVisible(true);
+    }
+    
+}
---
+++ Boletin30/Ejercicio1_Temporizador/Temporizador.java
@@ -0,0 +1,82 @@
+/**
+ * Objetivo: Crear un programa que utilice un javax.swing.Timer para ejecutar
+una tarea repetitiva cada cierto intervalo de tiempo. La tarea será mostrar
+un mensaje personalizado en la consola. Este mensaje puede variar
+dependiendo de una condición específica definida por el usuario.
+Utilizaremos una clase anónima para definir la acción que realizará el
+temporizador.
+ */
+package tareaca_temporizador;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Temporizador extends JFrame{
+    private JButton boton;
+    private JPanel panel;
+    private Timer temporizador;
+    private String mensaje;
+    private int contador;
+    private boolean activa = false;
+    
+    public Temporizador(){
+        super("TEMPORIZADOR");
+        setSize(400,300);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        
+        //Llamamos a los métodos
+        colocamosBoton();
+        panel();
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    private void panel(){
+        panel = new JPanel();
+        //Añadimos el panel a la ventana
+        this.getContentPane().add(panel);
+        panel.setLayout(new BorderLayout());
+    }
+    
+    private void colocamosBoton(){
+        boton = new JButton();
+        boton.setBounds(200,200,100,40);
+        boton.setText("Click, para ver el mensaje");
+        add(boton,BorderLayout.CENTER);
+        getContentPane().add(boton);
+        
+        boton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if(!activa){
+                    colocamosTemporizador();
+                }
+            }
+        });
+        
+    }
+    
+    private void colocamosTemporizador(){
+        //Colocamos el temporizador con los miliseg
+        temporizador = new Timer(2000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                contador++;
+        
+                if(contador <2){
+                    JOptionPane.showMessageDialog(null, "¡Hola!");
+                } else if(contador >=2 && contador <=4){
+                    JOptionPane.showMessageDialog(null, "Levantante!");
+                } else if(contador >=4 && contador <=6){
+                    JOptionPane.showMessageDialog(null, "y toma cafecito UwU");
+                    temporizador.stop();
+                }
+            }
+        });
+        temporizador.start();
+    }    
+}
